aaf9e787f689fb38ab96a2c88ac51eaa21933710
Administrator <admin@example.com>
2024-08-03 09:29:37 +0800

    项目初始化

---
+++ gulimall-product/src/test/java/com/wrp/gulimall/product/GulimallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wrp.gulimall.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
